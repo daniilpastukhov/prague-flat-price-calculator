c06c8283f69a455f4c3a65030089ab877459b1b6
Daniil Pastukhov <pastudan@fit.cvut.cz>
2019-08-16 11:36:05 +0500

    make it better

---
+++ frontend/dist/js/about.4416ad35.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{fcd1:function(t,n,e){"use strict";e.r(n);var s=function(){var t=this,n=t.$createElement;t._self._c;return t._m(0)},c=[function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",{staticClass:"statistics"},[e("h1",[t._v("Coming soon...")])])}],a=e("2877"),i={},l=Object(a["a"])(i,s,c,!1,null,null,null);n["default"]=l.exports}}]);
+//# sourceMappingURL=about.4416ad35.js.map
